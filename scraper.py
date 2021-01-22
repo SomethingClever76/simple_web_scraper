989ccd34a33f479b6bc003b5485ee180256cddeb
Kyle <mrkylethomas76@gmail.com>
2021-01-22 12:52:37 -0500

    Implement find_phone_nums/works but needs de-duping and formatting

--- scraper.py
+++ scraper.py
@@ -6,9 +6,10 @@ in the page.
 """
 
 __author__ = """Kyle Thomas, with the aid of:
+             https://requests.readthedocs.io/en/master/user/quickstart/
              https://docs.python.org/3/library/re.html
              https://regex101.com/
-             https://requests.readthedocs.io/en/master/user/quickstart/
+             http://phoneregex.com/
              http://urlregex.com/
              """
 
@@ -25,11 +26,20 @@ def retrieve_text(url):
 
 
 def find_urls(text):
-    """Creates a list of URLs from a webpage by using regex to search HTML"""
+    """Creates a list of URLs included on a webpage"""
     url_regex = [r'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|',
                  r'(?:%[0-9a-fA-F][0-9a-fA-F]))+']
     urls = re.findall(''.join(url_regex), text)
     for item in urls:
+        print(item)
+
+
+def find_phone_nums(text):
+    """Creates a list of phone numbers included on a webpage"""
+    phone_regex = [r'1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})',
+                   r'\W*([0-9]{4})(\se?x?t?(\d*))?']
+    ph_nums = (re.findall(''.join(phone_regex), text))
+    for item in ph_nums:
         print(item)
 
 
@@ -46,8 +56,8 @@ def main(args):
     ns = parser.parse_args(args)
 
     text = retrieve_text(ns.url)
-
     find_urls(text)
+    find_phone_nums(text)
 
 
 if __name__ == '__main__':
